495d149a82e23a633c2f2ce8f882ad575bfa0c39
Dante Trevisan <dante.trevisan248@gmail.com>
2024-07-20 19:40:12 -0300

    primer commit

---
+++ js/script.js
@@ -0,0 +1,8 @@
+let numero1 = parseInt(prompt("INgrese el primer numero"));
+let numero2 = parseInt(prompt("INgrese el segundo numero (DISTINTO DE CERO"));
+
+console.log(`La suma de ${numero1} y ${numero2} es ${numero1+numero2}`);
+console.log(`La resta de ${numero1} y ${numero2} es ${numero1-numero2}`);
+console.log(`La multiplicacion de ${numero1} y ${numero2} es ${numero1*numero2}`);
+console.log(`La division de ${numero1} y ${numero2} es ${numero1/numero2}`);
+
