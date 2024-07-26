833a729358ac3423fa7947100043ad8e84ddcdab
Dante Trevisan <dante.trevisan248@gmail.com>
2024-07-26 10:46:23 -0300

    cambie el titulo de las funiones y aclare en los console que funcion uso (funcion comun, funcion flecha, con o sin argumentos)

--- js/script.js
+++ js/script.js
@@ -1,19 +1,36 @@
-function sumar(){
+function sumarSinArgumentos(){
     let numero1 = 5;
     let resultado = numero1+5;
 
     return resultado
 }
 
-console.log("Funcion escrita normal")
-console.log(sumar())
+console.log("Funcion escrita normal sin argumentos")
+console.log(sumarSinArgumentos())
 
-let sumar2 = () => {
+let sumarFlechaSinArgumentos = () => {
     let numero1 = 5;
     let resultado = numero1+5;
 
     return resultado; 
 }
 
-console.log("Funcion flecha");
-console.log(sumar2());
+console.log("Funcion flecha sin argumentos");
+console.log(sumarFlechaSinArgumentos());
+
+function sumarConArgumentos(numero1, numero2){
+    let resultado = numero1+numero2;
+    return resultado;
+}
+
+console.log("Funcion con argumentos");
+console.log(sumarConArgumentos(100,50));
+
+let sumarFlechaConArgumentos = (numero1, numero2) => {
+    let resultado = numero1+numero2;
+    return resultado; 
+}
+
+console.log("Funcion flecha con argumentos");
+console.log(sumarFlechaConArgumentos(200,300));
+
