c7b8aa1b04cc81cd151c875b8ecf96c8211f2486
Dante Trevisan <dante.trevisan248@gmail.com>
2024-07-26 10:38:31 -0300

    agregue funcion flecha

--- js/script.js
+++ js/script.js
@@ -1,10 +1,19 @@
 function sumar(){
     let numero1 = 5;
-    let numero2 = numero1+5;
-
-    let resultado = numero1 + numero2;
+    let resultado = numero1+5;
 
     return resultado
 }
 
+console.log("Funcion escrita normal")
 console.log(sumar())
+
+let sumar2 = () => {
+    let numero1 = 5;
+    let resultado = numero1+5;
+
+    return resultado; 
+}
+
+console.log("Funcion flecha");
+console.log(sumar2());
